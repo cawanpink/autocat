4e9cf4914296ed2369ac222f837c6bbce25281bc
nuhaa <cawan.nuhaa@gmail.com>
2015-07-29 13:05:15 +0800

    meh

---
+++ meta_score.py
@@ -0,0 +1,7 @@
+
+class MetaScore(object):
+    def __init__(self):
+        pass
+
+    def get_score(self):
+        return 0.4
---
+++ tags_score.py
@@ -0,0 +1,6 @@
+
+class TagsScore(object):
+    def __init__():
+        pass
+    def get_score(self):
+        return 0.8
