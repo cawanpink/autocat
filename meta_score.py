0ae694a555dd880a4f965f718a3646671f79f4f1
nuhaa <cawan.nuhaa@gmail.com>
2015-07-29 14:03:38 +0800

    lo

--- meta_score.py
+++ meta_score.py
@@ -1,7 +1,29 @@
+import os
 
 class MetaScore(object):
     def __init__(self):
         pass
 
     def get_score(self):
-        return 0.4
+#        base_dir = os.path.abspath(__file__)
+
+        search_terms = open('industries/automotive/search_terms.csv')
+#        search_terms = open(base_dir + '/industries/automotive/search_terms.csv')
+        p = 0
+        terms = []
+        for term in search_terms:
+            terms.append(term)
+            p = p+1
+            
+        meta_keywords = open('results/automotive/meta.csv')
+        count = 0
+        for keyword in meta_keywords:
+            words = keyword.split(',')
+
+        for word in words:
+            for term in terms:
+                if term == word:
+                    count = count + 1
+
+        meta_score = int(count) / float(len(terms))
+        return  float(meta_score)
---
+++ scoring.py
@@ -0,0 +1,8 @@
+from meta_score import MetaScore
+from tags_score import TagsScore
+from imagga import ImaggaClient
+
+
+
+m = MetaScore()
+print m.get_score()
